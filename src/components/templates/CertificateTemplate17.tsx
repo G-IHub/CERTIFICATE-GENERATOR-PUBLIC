c80e637d697ef7a8de7e7a6c7d40655ac90f0008
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2026-04-09 10:28:29 +0100

    Fix 05

--- src/components/templates/CertificateTemplate17.tsx
+++ src/components/templates/CertificateTemplate17.tsx
@@ -1,9 +1,6 @@
 import { useRef, useEffect } from "react";
 import watermark from "../../assets/watermark.png";
-import award from "../../assets/award.png";
 import barcode from "../../assets/barcode.png";
-import nig from "../../assets/nig.png";
-import usa from "../../assets/usa.png";
 
 interface CertificateTemplate17Props {
   header: string;
@@ -146,81 +143,83 @@ export default function CertificateTemplate17({
               </p> */}
 
             <div className="flex justify-between items-end mt-10 relative">
-              <div className="w-[40px]">
-                <img src={barcode} alt="barcode" className="w-full" />
-              </div>
-
-              <div className="absolute left-[270px] top-[60%] transform -translate-x-1/2 -translate-y-1/2 z-20">
+              {/* <div className="absolute left-[270px] top-[60%] transform -translate-x-1/2 -translate-y-1/2 z-20">
                 <img
                   src={award}
                   alt="award"
                   className="w-[200px] object-contain opacity-100"
                 />
+              </div> */}
+
+              {/* Left Side: Barcode and Signature 1 */}
+              <div className="flex items-end gap-6 pl-4">
+                <div className="w-[40px] pb-2">
+                  <img src={barcode} alt="barcode" className="w-full" />
+                </div>
+
+                {/* Signature 1 - Always show if name is provided */}
+                {signatoryName1 && (
+                  <div className="flex flex-col items-center text-center px-2">
+                    {signatureUrl1 && (
+                      <img
+                        src={signatureUrl1}
+                        alt={signatoryName1}
+                        className="w-24 h-16 object-contain"
+                        style={{ marginBottom: -12 }}
+                      />
+                    )}
+                    {!signatureUrl1 && (
+                      <div className="w-32 border-b-2 border-gray-400 mb-2" />
+                    )}
+                    <div className="text-sm font-bold text-black">
+                      {signatoryName1}
+                    </div>
+                    {signatoryTitle1 && (
+                      <div className="text-xs font-medium">
+                        {signatoryTitle1}
+                      </div>
+                    )}
+                  </div>
+                )}
               </div>
 
-              <div className="flex justify-between items-end">
-                <div className="flex gap-1 justify-between items-center">
-                  {/* Signature 1 - Always show if name is provided */}
-                  {signatoryName1 && (
-                    <div className="flex flex-col items-center text-center px-5 mx-1">
-                      {signatureUrl1 && (
-                        <img
-                          src={signatureUrl1}
-                          alt={signatoryName1}
-                          className="w-24 h-16 object-contain"
-                          style={{ marginBottom: -12 }}
-                        />
-                      )}
-                      {!signatureUrl1 && (
-                        <div className="w-32 border-b-2 border-gray-400 mb-2" />
-                      )}
-                      <div className="text-sm font-bold text-black">
-                        {signatoryName1}
-                      </div>
-                      {signatoryTitle1 && (
-                        <div className="text-xs font-medium">
-                          {signatoryTitle1}
-                        </div>
-                      )}
+              {/* Right Side: Signature 2 and Date */}
+              <div className="flex items-end gap-6 pr-4">
+                {/* Signature 2 - Always show if name is provided */}
+                {signatoryName2 && (
+                  <div className="flex flex-col items-center text-center px-2">
+                    {signatureUrl2 && (
+                      <img
+                        src={signatureUrl2}
+                        alt={signatoryName2}
+                        className="w-24 h-16 object-contain"
+                        style={{ marginBottom: -12 }}
+                      />
+                    )}
+                    {!signatureUrl2 && (
+                      <div className="w-32 border-b-2 border-gray-400 mb-2" />
+                    )}
+                    <div className="text-sm font-bold text-black">
+                      {signatoryName2}
                     </div>
-                  )}
+                    {signatoryTitle2 && (
+                      <div className="text-xs font-medium">
+                        {signatoryTitle2}
+                      </div>
+                    )}
+                  </div>
+                )}
 
-                  {/* Signature 2 - Always show if name is provided */}
-                  {signatoryName2 && (
-                    <div className="flex flex-col items-center text-center px-5 mx-1">
-                      {signatureUrl2 && (
-                        <img
-                          src={signatureUrl2}
-                          alt={signatoryName2}
-                          className="w-24 h-16 object-contain"
-                          style={{ marginBottom: -12 }}
-                        />
-                      )}
-                      {!signatureUrl2 && (
-                        <div className="w-32 border-b-2 border-gray-400 mb-2" />
-                      )}
-                      <div className="text-sm font-bold text-black">
-                        {signatoryName2}
-                      </div>
-                      {signatoryTitle2 && (
-                        <div className="text-xs font-medium">
-                          {signatoryTitle2}
-                        </div>
-                      )}
+                {/* Date display */}
+                {date && (
+                  <div className="flex flex-col items-center text-center px-2">
+                    <div className="w-32 mt-7 mb-2" />
+                    <div className="text-xs font-bold ">Date</div>
+                    <div className="text-sm font-medium text-black">
+                      {formattedDate || "DATE"}
                     </div>
-                  )}
-
-                  {/* Date display */}
-                  {date && (
-                    <div className="flex flex-col items-center text-center">
-                      <div className="w-32 mt-7 mb-2" />
-                      <div className="text-xs font-bold ">Date</div>
-                      <div className="text-sm font-medium text-black">
-                        {formattedDate || "DATE"}
-                      </div>
-                    </div>
-                  )}
-                </div>
+                  </div>
+                )}
               </div>
             </div>
           </div>
